aa52b7df53f62b40724a2a2aafb5d49fbe4167c7
Jeanca <jeancarlosbarberena29@gmail.com>
2023-09-01 18:29:11 -0600

    Agregar usuario casi terminadito

---
+++ Clases/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29MoneyCalculator.Clases
+{
+    public class User
+    {
+        public int User_Id { get; set; }
+        public string User_Name { get; set; }
+        public string User_last_name { get; set; }
+        public string User_Email { get; set; }
+        public string User_Password { get; set; }
+        public char User_Gender { get; set; }
+        public DateTime User_Birth { get; set; }
+        public string User_Profile_Image { get; set; }
+    }
+}
---
+++ Controladores/UserController.cs
@@ -0,0 +1,25 @@
+﻿using _29MoneyCalculator.Clases;
+using _29MoneyCalculator.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29MoneyCalculator.Controladores
+{
+    public class UserController
+    {
+        private UserModel userModel;
+
+        public UserController()
+        {
+            userModel = new UserModel();
+        }
+        public void CreateUser(User user)
+        {
+            userModel.CreateUser(user);
+        }
+    }
+}
