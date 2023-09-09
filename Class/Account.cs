c51caceb14b14bb7d238bdc3f68d5a2346d6e912
Jeanca <jeancarlosbarberena29@gmail.com>
2023-09-09 13:28:48 -0600

    COMMIT

---
+++ Class/Account.cs
@@ -0,0 +1,18 @@
+﻿using _29_Money_Calculator.BD;
+
+namespace _29MoneyCalculator.Class
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string TitleAccount { get; set; }
+        public string DescriptionAccount { get; set; }
+        public decimal Earnings { get; set; }
+        public decimal Taxes { get; set; }
+        public decimal Bills { get; set; }
+        public decimal Leisure { get; set; }
+        public int User_ID { get; set; }
+
+
+    }
+}
